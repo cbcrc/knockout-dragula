3006add6109a87637b1c0e704c311c3593508825
Laurent le Beau-Martin <laurent.lebeau-martin@radio-canada.ca>
2015-08-17 11:35:14 -0400

    Clean up

--- dist/knockout-dragula.js
+++ dist/knockout-dragula.js
@@ -19,37 +19,39 @@
 
   var _dragula2 = _interopRequireDefault(_dragula);
 
+  function makeTemplateOptions(valueAccessor) {
+    var templateOptions = {
+      foreach: valueAccessor()
+    };
+
+    return function () {
+      return templateOptions;
+    };
+  }
+
   _ko['default'].bindingHandlers.dragula = {
     init: function init(element, valueAccessor, allBindingsAccessor, data, context) {
       var items = valueAccessor();
 
-      var templateOptions = {
-        foreach: items
-      };
-      _ko['default'].bindingHandlers.template.init(element, function () {
-        return templateOptions;
-      }, allBindingsAccessor, data, context);
+      _ko['default'].bindingHandlers.template.init(element, makeTemplateOptions(valueAccessor), allBindingsAccessor, data, context);
 
       var drake = (0, _dragula2['default'])([element]);
       drake.on('drop', function (el, target /*, source*/) {
         var item = _ko['default'].dataFor(el);
         var sourceIndex = items.indexOf(item);
-        var targetIndex = Array.from(target.children).indexOf(el);
+        var targetIndex = Array.prototype.indexOf.call(target.children, el);
 
         items.splice(sourceIndex, 1);
         items.splice(targetIndex, 0, item);
         el.remove();
       });
+
+      _ko['default'].utils.domNodeDisposal.addDisposeCallback(element, function () {
+        drake.destroy();
+      });
     },
     update: function update(element, valueAccessor, allBindingsAccessor, data, context) {
-      var items = valueAccessor();
-      var templateOptions = {
-        foreach: items
-      };
-
-      _ko['default'].bindingHandlers.template.update(element, function () {
-        return templateOptions;
-      }, allBindingsAccessor, data, context);
+      _ko['default'].bindingHandlers.template.update(element, makeTemplateOptions(valueAccessor), allBindingsAccessor, data, context);
     }
   };
 });
--- knockout-dragula.js
+++ knockout-dragula.js
@@ -3,36 +3,36 @@
 import ko from 'knockout';
 import dragula from 'dragula';
 
+function makeTemplateOptions(valueAccessor) {
+  let templateOptions = {
+    foreach: valueAccessor()
+  };
+
+  return () => templateOptions;
+}
+
 ko.bindingHandlers.dragula = {
   init: function(element, valueAccessor, allBindingsAccessor, data, context) {
     let items = valueAccessor();
 
-    let templateOptions = {
-      foreach: items
-    };
-    ko.bindingHandlers.template.init(element, function() {
-      return templateOptions;
-    }, allBindingsAccessor, data, context);
+    ko.bindingHandlers.template.init(element, makeTemplateOptions(valueAccessor), allBindingsAccessor, data, context);
 
     let drake = dragula([element]);
-    drake.on('drop', function(el, target/*, source*/) {
+    drake.on('drop', function(el, target /*, source*/ ) {
       let item = ko.dataFor(el);
       let sourceIndex = items.indexOf(item);
-      let targetIndex = Array.from(target.children).indexOf(el);
+      let targetIndex = Array.prototype.indexOf.call(target.children, el);
 
       items.splice(sourceIndex, 1);
       items.splice(targetIndex, 0, item);
       el.remove();
     });
+
+    ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+      drake.destroy();
+    });
   },
   update: function(element, valueAccessor, allBindingsAccessor, data, context) {
-    let items = valueAccessor();
-    let templateOptions = {
-      foreach: items
-    };
-
-    ko.bindingHandlers.template.update(element, function() {
-      return templateOptions;
-    }, allBindingsAccessor, data, context);
+    ko.bindingHandlers.template.update(element, makeTemplateOptions(valueAccessor), allBindingsAccessor, data, context);
   }
 };
